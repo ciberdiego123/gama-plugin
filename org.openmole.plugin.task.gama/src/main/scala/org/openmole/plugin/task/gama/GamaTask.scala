0b7a719a91cdb8a076710f156e770a9f30d57e94
Romain Reuillon <romain.reuillon@iscpif.fr>
2015-04-21 23:27:46 +0200

    Use one single resource.

--- org.openmole.plugin.task.gama/src/main/scala/org/openmole/plugin/task/gama/GamaTask.scala
+++ org.openmole.plugin.task.gama/src/main/scala/org/openmole/plugin/task/gama/GamaTask.scala
@@ -14,7 +14,7 @@ import scala.collection.mutable.ListBuffer
 
 object GamaTask {
 
-  class Builder(gamlPath: String, experimentName: String, steps: Int) extends ExternalTaskBuilder { builder ⇒
+  class Builder(gamlPath: String, experimentName: String, steps: Int, workdir: String = "") extends ExternalTaskBuilder { builder ⇒
     private var gamaInputs = new ListBuffer[(Prototype[_], String)]
     private var gamaOutputs = new ListBuffer[(String, Prototype[_])]
     private var gamaVariableOutputs = new ListBuffer[(String, Prototype[_])]
@@ -56,7 +56,7 @@ object GamaTask {
     }
 
     def toTask =
-      new GamaTask(gamlPath, experimentName, steps, gamaInputs, gamaOutputs, gamaVariableOutputs, gamaSeed) with builder.Built {
+      new GamaTask(gamlPath, experimentName, steps, gamaInputs, gamaOutputs, gamaVariableOutputs, gamaSeed, workdir) with builder.Built {
         override val inputs = super.inputs + seed
       }
 
@@ -70,8 +70,8 @@ object GamaTask {
   }
 
   def apply(workspace: File, model: String, experimentName: String, steps: Int) = {
-    val b = new Builder(model, experimentName, steps)
-    workspace.listFiles.foreach (f => b addResource (f))
+    val b = new Builder(model, experimentName, steps, workspace.getName)
+    b addResource workspace
     b
   }
 
@@ -88,7 +88,8 @@ abstract class GamaTask(
     val gamaInputs: Iterable[(Prototype[_], String)],
     val gamaOutputs: Iterable[(String, Prototype[_])],
     val gamaVariableOutputs: Iterable[(String, Prototype[_])],
-    val seed: Prototype[Long]) extends ExternalTask {
+    val seed: Prototype[Long],
+    val workdir: String) extends ExternalTask {
 
 
   override protected def process(context: Context): Context = withWorkDir { tmpDir ⇒
@@ -96,7 +97,7 @@ abstract class GamaTask(
 
       GamaTask.preload
 
-      prepareInputFiles(context, tmpDir.getCanonicalFile, "")
+      prepareInputFiles(context, tmpDir.getCanonicalFile, workdir)
       val model = MoleSimulationLoader.loadModel(tmpDir.child(gaml))
 
       val experiment = MoleSimulationLoader.newExperiment(model)
@@ -121,7 +122,7 @@ abstract class GamaTask(
               }
           )
 
-        fetchOutputFiles(returnContext, tmpDir.getCanonicalFile, "")
+        fetchOutputFiles(returnContext, tmpDir.getCanonicalFile, workdir)
       } finally experiment.dispose
     }catch {
       case t: Throwable ⇒
