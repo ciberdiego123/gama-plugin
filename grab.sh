16a4ad208cfc8e8bcace47201a428c7e272cd684
Romain Reuillon <romain.reuillon@iscpif.fr>
2016-05-20 22:33:07 +0700

    Remove some quotes.

--- grab.sh
+++ grab.sh
@@ -16,45 +16,45 @@ LOCAL=.
 
 for f in `cat bundles.list`
 do
-  ls -d "$GAMA/$f"
+  ls -d $GAMA/$f
   if [[ $? != 0 ]]; then exit 1; fi
 done
 
-BUNDLES="$LOCAL/bundles/"
-LIBS="$LOCAL/lib"
+BUNDLES=$LOCAL/bundles/
+LIBS=$LOCAL/lib
 
 rm -rf "$BUNDLES" "$LIBS"
 
-mkdir -p "$BUNDLES"
-mkdir -p "$LIBS"
+mkdir -p $BUNDLES
+mkdir -p $LIBS
 
 for f in `cat bundles.list`
 do
-  cp -r "$GAMA/$f" "$BUNDLES/"
+  cp -r $GAMA/$f $BUNDLES/
 done
 
-cd "$BUNDLES"
+cd $BUNDLES
 
 DIRS=`find . -mindepth 1 -maxdepth 1 -type d`
 
-for d in "$DIRS"
+for d in $DIRS
 do 
   NAME=`ls -d $d`
   if [[ $? != 0 ]]; then exit 1; fi
   jar -cvfM $NAME.jar -C $NAME .
-  mv "$NAME/*" "../$LIBS"
+  mv $NAME/* ../$LIBS
   rm -rf "$NAME"
 done
 
 cd -
-cd "$LIBS"
+cd $LIBS
 
 for f in `ls ../bundles`
 do
-  ln -s "../bundles/$f"
+  ln -s ../bundles/$f
 done
 
 cd -
 
-mv "$BUNDLES/org.apache.commons.logging_*.jar" "$LIBS"
-mv "$BUNDLES/org.apache.log4j_*.jar" "$LIBS"
+mv $BUNDLES/org.apache.commons.logging_*.jar $LIBS
+mv $BUNDLES/org.apache.log4j_*.jar $LIBS
