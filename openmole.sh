bf46b534a9d0c019b298e18f72596e277a2a5965
s.rey.coyrehourcq <s.rey.coyrehourcq@gmail.com>
2017-05-24 16:40:33 +0200

    fix openmole compilation

--- openmole.sh
+++ openmole.sh
@@ -4,7 +4,7 @@ git clone https://github.com/openmole/openmole.git --branch dev
 cd openmole
 git submodule init
 git submodule update
-cd ../build-system
+cd build-system
 sbt publish-local
 cd ../libraries
 sbt publish-local
